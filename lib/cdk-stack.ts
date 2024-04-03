207305fdc3c1ae4084bf9563c14241ca30d3818a
namanchandak <naman.chandak@hotmail.com>
2024-04-04 00:14:11 +0530

    error

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -2,12 +2,32 @@ import * as cdk from 'aws-cdk-lib';
 import { ApiGateway } from './ApiGateway';
 import { Construct } from 'constructs';
 import { Lambda } from './Lambda';
+import { PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import iam = require('aws-sdk/clients/iam');
 
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const lambdaRole = new Role(this, 'LambdaExecutionRole', {
+      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+    });
+
+    
+
+    lambdaRole.addToPolicy(new iam.PolicyStatement({
+      effect : iam.Effect.ALLOW,
+      actions: [ 'logs:CreateLogStream', 'logs:PutLogEvents', 'logs:CreateLogGroup'],
+      resources: ['*'] 
+    }));
+
+    const apiGatewayRole = new Role(this, 'ApiGatewayExecutionRole', {
+      assumedBy: new ServicePrincipal('apigateway.amazonaws.com'),
+    });
+    
+
+    
     //api
     
     const api = new ApiGateway(this);
