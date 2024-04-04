4ad25d07358b36be49ed064b62ed7c37aef4a48b
namanchandak <naman.chandak@hotmail.com>
2024-04-04 18:51:58 +0530

    get all user

--- backend/lambda/db.ts
+++ backend/lambda/db.ts
@@ -2,11 +2,7 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import {
   DynamoDBDocumentClient,
   ScanCommand,
-  PutCommand,
-  GetCommand,
-  DeleteCommand,
 } from "@aws-sdk/lib-dynamodb";
-import { error } from "console";
 
 const client = new DynamoDBClient({});
 
@@ -14,7 +10,11 @@ const dynamo = DynamoDBDocumentClient.from(client);
 
 const tableName = "MyTable";
 
-export const handler = async (event: { routeKey: any; pathParameters: { id: any; }; body: string; }) => {
+export const handler = async (event: {
+  routeKey: any;
+  pathParameters: { id: any };
+  body: string;
+}) => {
   let body;
   let statusCode = 200;
   const headers = {
@@ -22,56 +22,16 @@ export const handler = async (event: { routeKey: any; pathParameters: { id: any;
   };
 
   try {
-    switch (event.routeKey) {
-      case "DELETE /items/{id}":
-        await dynamo.send(
-          new DeleteCommand({
-            TableName: tableName,
-            Key: {
-              id: event.pathParameters.id,
-            },
-          })
-        );
-        body = `Deleted item ${event.pathParameters.id}`;
-        break;
-      case "GET /items/{id}":
-        body = await dynamo.send(
-          new GetCommand({
-            TableName: tableName,
-            Key: {
-              id: event.pathParameters.id,
-            },
-          })
-        );
-        body = body.Item;
-        break;
-      case "GET /items":
-        body = await dynamo.send(
-          new ScanCommand({ TableName: tableName })
-        );
-        body = body.Items;
-        break;
-      case "PUT /items":
-        let requestJSON = JSON.parse(event.body);
-        await dynamo.send(
-          new PutCommand({
-            TableName: tableName,
-            Item: {
-              id: requestJSON.id,
-              price: requestJSON.price,
-              name: requestJSON.name,
-            },
-          })
-        );
-        body = `Put item ${requestJSON.id}`;
-        break;
-      default:
-        throw new Error(`Unsupported route: "${event.routeKey}"`);
-    }
+    body = await dynamo.send(new ScanCommand({ TableName: tableName }));
+    body = body.Items;
   } catch (err) {
+
     statusCode = 400;
-    body = "error";
+    console.log(err);
+
+    body = JSON.stringify({ error: err });
   } finally {
+
     body = JSON.stringify(body);
   }
 
