ea17252a2c5a46470712199b3ef37efcf3eabd0f
namanchandak <naman.chandak@hotmail.com>
2024-04-09 18:26:21 +0530

    test updated

--- test/getAllUsers.test.ts
+++ test/getAllUsers.test.ts
@@ -1,19 +1,8 @@
 const lambdaHandler = require('../backend/lambda/getAllUsers'); 
 
 describe('Lambda Function Tests', () => {
-    test('Test Case 1: Checking Database Connection', async () => {
-        const event = {
-            routeKey: 'test',
-            pathParameters: { id: 'testId' },
-            body: 'testBody'
-        };
-        const response = await lambdaHandler.handler(event);
-        
-        expect(response.statusCode).toBe(200);
-        expect(response.body).not.toBeUndefined();
-    });
-
-    test('Test Case 2: Retrieving Data for All Users', async () => {
+    
+    test('Test Case 1: Retrieving Data for All Users', async () => {
 
         const event = {
             routeKey: 'test',
--- test/getUserById.test.ts
+++ test/getUserById.test.ts
@@ -1,42 +1,34 @@
 const lambdaHandler = require('../backend/lambda/getById'); 
-const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
-
-jest.mock("@aws-sdk/client-dynamodb", () => ({
-    DynamoDBClient: jest.fn(() => ({
-        send: jest.fn(),
-    })),
-    ScanCommand: jest.fn(),
-}));
 
 describe('Lambda Function Tests', () => {
-    test('Test Case 1: Checking Database Connection', async () => {
-        const response = await lambdaHandler.handler({ routeKey: 'test', pathParameters: { id: '1' }, body: 'testBody' });
+    
+
+    test('Test Case 1: Retrieving Data for a Specific User', async () => {
+        const mockScanOutput = {
+            Items: [
+                { id: { N: '1' }, name: { S: 'User1' } },
+                { id: { N: '2' }, name: { S: 'User2' } },
+            ]
+        };
+
+        const event = {
+            routeKey: 'test',
+            pathParameters: { id: '1' }, 
+            body: ''
+        };
+        const response = await lambdaHandler.handler(event);
 
         expect(response.statusCode).toBe(200);
         expect(response.body).not.toBeUndefined();
+        const responseBody = JSON.parse(response.body);
+
+        expect(responseBody).toEqual({
+            "id": {
+              "N": "1"
+            },
+            "name": {
+              "S": "chris"
+            }
+          });
     });
-
-    // test('Test Case 2: Retrieving Data for a Specific User', async () => {
-    //     const mockScanOutput = {
-    //         Items: [
-    //             { id: { N: '1' }, name: { S: 'User1' } },
-    //             { id: { N: '2' }, name: { S: 'User2' } },
-    //             // Add more test data as needed
-    //         ]
-    //     };
-    //     DynamoDBClient.prototype.send.mockResolvedValue({ 
-    //         $metadata: { httpStatusCode: 200 }, 
-    //         Items: mockScanOutput.Items 
-    //     });
-
-    //     const event = {
-    //         routeKey: 'test',
-    //         pathParameters: { id: '1' }, 
-    //         body: 'testBody'
-    //     };
-    //     const response = await lambdaHandler.handler(event);
-
-    //     expect(response.statusCode).toBe(200);
-    //     expect(response.body).not.toBeUndefined();
-    // });
 });
