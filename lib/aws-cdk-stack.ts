54d0590603b98ba619ab805147281b8a0931c2a3
namanchandak <naman.chandak@hotmail.com>
2024-04-30 18:06:43 +0530

    creating db and lambda with cdk

--- lib/aws-cdk-stack.ts
+++ lib/aws-cdk-stack.ts
@@ -3,6 +3,9 @@ import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
+
 
 export class AwsCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -15,5 +18,17 @@ export class AwsCdkStack extends Stack {
     const topic = new sns.Topic(this, 'AwsCdkTopic');
 
     topic.addSubscription(new subs.SqsSubscription(queue));
+
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,    
+      code: lambda.Code.fromAsset('lambda'),  
+      handler: 'hello.handler'                
+    });
+
+    const table = new Table(this, 'DTable', {
+      tableName: "DTable",
+      partitionKey: { name: 'id', type: AttributeType.NUMBER },
+      
+    });
   }
 }
